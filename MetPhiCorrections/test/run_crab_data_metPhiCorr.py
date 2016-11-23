7020dfed113e62af691cfddd0dd459011b7a9047
Ece Asilar <ece.asilar@cern.ch>
2016-11-23 11:50:46 +0100

    new rereco data cfi

--- MetPhiCorrections/test/run_crab_data_metPhiCorr.py
+++ MetPhiCorrections/test/run_crab_data_metPhiCorr.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config , getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'METPhiCorr_analysis_Data_80_G_GTCorr_updatelumi'
+config.General.requestName = 'METPhiCorr_analysis_Data_80_B_GTCorr_updatelumi'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -10,19 +10,21 @@ config.JobType.allowUndistributedCMSSW = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'run_metPhiCorr.py'
 
-#config.Data.inputDataset = '/DoubleMuon/Run2016B-PromptReco-v2/MINIAOD'
-#config.Data.inputDataset = '/DoubleMuon/Run2016C-PromptReco-v2/MINIAOD'
-#config.Data.inputDataset = '/DoubleMuon/Run2016D-PromptReco-v2/MINIAOD'
-#config.Data.inputDataset = '/DoubleMuon/Run2016E-PromptReco-v2/MINIAOD'
-#config.Data.inputDataset = '/DoubleMuon/Run2016F-PromptReco-v1/MINIAOD'
-config.Data.inputDataset = '/DoubleMuon/Run2016G-PromptReco-v1/MINIAOD'
+config.Data.inputDataset = '/DoubleMuon/Run2016B-23Sep2016-v3/MINIAOD'
+##config.Data.inputDataset = '/DoubleMuon/Run2016C-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016D-23Sep2016-v1/MINIAOD'
+##config.Data.inputDataset = '/DoubleMuon/Run2016E-23Sep2016-v1/MINIAOD'
+##config.Data.inputDataset = '/DoubleMuon/Run2016F-23Sep2016-v1/MINIAOD'
+##config.Data.inputDataset = '/DoubleMuon/Run2016G-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016G-PromptReco-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
-config.Data.lumiMask = 'Cert_271036-280385_13TeV_PromptReco_Collisions16_JSON_NoL1T_v2.txt'
+#config.Data.lumiMask = 'Cert_271036-280385_13TeV_PromptReco_Collisions16_JSON_NoL1T_v2.txt'
+config.Data.lumiMask = 'Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_Data_80X_G_GTCorr'
+config.Data.outputDatasetTag = 'CRAB3_Data_80X_GTCorr'
 
 config.Site.storageSite = 'T2_AT_Vienna'
 
--- MetPhiCorrections/test/run_metPhiCorr.py
+++ MetPhiCorrections/test/run_metPhiCorr.py
@@ -15,7 +15,8 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 #process.GlobalTag.globaltag = '80X_dataRun2_Prompt_ICHEP16JEC_v0' ##B,C,D
 #process.GlobalTag.globaltag = '80X_dataRun2_Prompt_v10'   ##E+F
-process.GlobalTag.globaltag = '80X_dataRun2_Prompt_v11'   ##G
+#process.GlobalTag.globaltag = '80X_dataRun2_Prompt_v11'   ##G
+process.GlobalTag.globaltag = '80X_dataRun2_2016SeptRepro_v4'
 
 process.load('MetTools.MetPhiCorrections.phiCorr_80X_cff')
 #Replacements for mAOD
